b58e11ce803058933ba21fb23547eb176ede213d
sandipmryadav <151117657+sandipmryadav@users.noreply.github.com>
2026-06-24 23:02:00 +0530

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FreelanceHub.Models;
+namespace FreelanceHub.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FreelanceHub.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(150)]
+        public string Email { get; set; }
+
+        [Required]
+        public string PasswordHash { get; set; }
+
+        public string Role { get; set; } = "User";
+        public DateTime CreatedAt { get; set; } = DateTime.Now; 
+
+    }
+}
